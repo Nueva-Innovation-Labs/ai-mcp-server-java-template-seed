fef40a314794d15e362df6053e2289a798f4f965
Sir John Nueva <sirjohn.nueva@gmail.com>
2025-11-05 22:14:10 +0800

    Base Code

---
+++ src/main/java/com/nuevainnovation/ai_mcp_server_java_template_seed/AiMcpServerJavaTemplateSeedApplication.java
@@ -0,0 +1,13 @@
+package com.nuevainnovation.ai_mcp_server_java_template_seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMcpServerJavaTemplateSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMcpServerJavaTemplateSeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuevainnovation/ai_mcp_server_java_template_seed/AiMcpServerJavaTemplateSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuevainnovation.ai_mcp_server_java_template_seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMcpServerJavaTemplateSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
